2f43d09095ee72b59f6f5f09ecb9c1c931ee0b71
nyanko4 <nyankobetsuaka@outlook.com>
2025-06-17 22:36:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/hello")
-  def hello(name: str):
-    return {"message": f"Hello {name}!"}
+def hello(name: str):
+  return {"message": f"Hello {name}!"}
