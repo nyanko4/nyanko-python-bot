17f2205ef2df32a5ec5d96e459fc3cb1442c2372
nyanko4 <nyankobetsuaka@outlook.com>
2025-06-21 12:03:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ app = FastAPI()
 
 @app.post("/webhook")
 async def webhook(request: Request):
-    body = await request.json()
+    req_body = await request.json()
+    data = req_body["webhook_event"]
+    body = data.get("body")
+    account_id = data.get("account_id")
+    room_id = data.get("room_id")
+    message_id = data.get("message_id")
+    send_time = data.get("send_time")
+    update_time = data.get("update_time")
     print(body)
