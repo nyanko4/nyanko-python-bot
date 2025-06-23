a8a47ea78fdd76a6f2aaea399e5e17b57a856033
nyanko4 <nyankobetsuaka@outlook.com>
2025-06-23 20:39:00 +0900

    Update message.py

--- ctr/message.py
+++ ctr/message.py
@@ -5,17 +5,17 @@ CHATWORK_TOKEN = os.getenv("CHATWORK_API_TOKEN")
 
 def sendchatwork(ms, roomId):
     try:
-        requests.post(
+        response = requests.post(
             f"https://api.chatwork.com/v2/rooms/{roomId}/messages", 
-            data={
-                "self_unread": 0,
-                "body": ms
-            },
-            headers={
-                "X-ChatWorkToken": CHATWORK_TOKEN,
-                "content-type": "application/x-www-form-urlencoded"
-                }
-        )
+                data={
+                    "self_unread": 0,
+                    "body": ms
+                },
+                headers={
+                    "X-ChatWorkToken": CHATWORK_TOKEN,
+                    "content-type": "application/x-www-form-urlencoded"
+                    }
+            )
         if response.status_code == 200:
             print("メッセージを送信しました")
         else:
