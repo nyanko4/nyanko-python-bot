7a98c83e10541ba740132128d70b89c7bce50f0d
nyanko4 <nyankobetsuaka@outlook.com>
2025-06-24 09:21:05 +0900

    Create _init_db.py

---
+++ db/_init_db.py
@@ -0,0 +1,19 @@
+import aiosqlite
+import asyncio
+
+async def init_db():
+    async with aiosqlite.connect("omikuji.db") as db:
+        await db.execute("""
+            CREATE TABLE IF NOT EXISTS omikuji (
+                accoutId INTEGER,
+                result TEXT,
+                roomId INTEGER,
+                name TEXT,
+                date NUMERIC
+            )
+        """)
+        await db.commit()
+        print("✅ おみくじテーブル作成完了")
+
+if __name__ == "__main__":
+    asyncio.run(init_db())
