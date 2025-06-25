62a6b8f8d16fa44088dfe699b5c1e01e13f9eff8
nyanko4 <nyankobetsuaka@outlook.com>
2025-06-25 13:51:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Request
 from db._init_db import _init_db
 from webhook.getchat import router as getchat
-from quiz.send import router as quiz
 
 app = FastAPI()
     
@@ -14,5 +13,3 @@ async def startup():
     await _init_db()
    
 app.include_router(getchat)
-
-app.include_router(quiz)
