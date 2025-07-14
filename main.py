fbb1c724791e20132d02d708fd21e8046d020ed8
nyanko4 <nyankobetsuaka@outlook.com>
2025-07-14 10:15:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, Request
-from db._init_db import _init_db
 from webhook.getchat import router as getchat
 
 app = FastAPI()
@@ -7,9 +6,5 @@ app = FastAPI()
 @app.get("/")
 def send():
     return {"status": "ok"}
-    
-@app.on_event("startup")
-async def startup():
-    await _init_db()
    
 app.include_router(getchat)
